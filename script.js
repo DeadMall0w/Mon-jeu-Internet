b06f913a3862e42cace0c7e5b2fb583e10e51271
DeadMall0w <100719400+DeadMall0w@users.noreply.github.com>
2024-02-19 19:12:38 +0100

    Add sprite drawing

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const canvas = document.getElementById('myCanvas');
+const canvas = document.getElementById('Canvas');
 const ctx = canvas.getContext('2d');
 
+// function resizeCanvas() {
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerWidth;
+// }
+// resizeCanvas();
+
 // Fonction pour créer une grille de la taille spécifiée
-function createGrid(rows, cols, squareSize) {
+
+let squareSize = 50; 
+function createGrid(rows, cols) {
     // Effacer le canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Dessiner la grille
+    squareSize = canvas.width/rows
     for (let row = 0; row < rows; row++) {
         for (let col = 0; col < cols; col++) {
-            ctx.strokeStyle = 'black';
+            ctx.strokeStyle = 'white';
             ctx.strokeRect(col * squareSize, row * squareSize, squareSize, squareSize);
         }
     }
@@ -22,18 +32,14 @@ function removeSquare(row, col, squareSize) {
     ctx.clearRect(col * squareSize, row * squareSize, squareSize, squareSize);
 }
 
-function drawSprite(sprite, row, col, squareSize) {
-    // Charger l'image sprite
+function drawSprite(sprite, row, col) {
     const img = new Image();
     img.src = sprite;
-
-    // Attendre le chargement de l'image
+ 
     img.onload = function() {
-        // Dessiner l'image dans la case spécifiée
         ctx.drawImage(img, col * squareSize, row * squareSize, squareSize, squareSize);
     };
 }
 
-drawSprite('sprite.png', 1, 1, 50);
-
-// removeSquare(1, 1, 50);
+drawSprite('Img/O.png', 1, 1);
+drawSprite('Img/x.png', 1, 4);
