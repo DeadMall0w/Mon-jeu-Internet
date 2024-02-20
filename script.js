d012962576a5915805821d63617d1f802ee6a550
Raphael Berquier <100719400+DeadMall0w@users.noreply.github.com>
2024-02-20 17:55:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,9 +9,24 @@ const ctx = canvas.getContext('2d');
 
 // Fonction pour créer une grille de la taille spécifiée
 
+class Pion {
+    constructor(x, y, player) {
+      this.x = x;
+      this.y = y;
+      this.player = player;
+    }
+
+    
+    changeValue(v){
+
+    }
+  }
+
 let squareSize = 50; 
+let width = 10;
+let height = 10;
+
 function createGrid(rows, cols) {
-    // Effacer le canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -25,7 +40,6 @@ function createGrid(rows, cols) {
     }
 }
 
-createGrid(8, 8, 50);
 
 
 function removeSquare(row, col, squareSize) {
@@ -35,11 +49,50 @@ function removeSquare(row, col, squareSize) {
 function drawSprite(sprite, row, col) {
     const img = new Image();
     img.src = sprite;
- 
+    
     img.onload = function() {
         ctx.drawImage(img, col * squareSize, row * squareSize, squareSize, squareSize);
     };
 }
 
-drawSprite('Img/O.png', 1, 1);
-drawSprite('Img/x.png', 1, 4);
+function CreateMap(){
+    map = []
+    for (var i = 0; i < width; i++) {
+        map[i] = [];
+        for (var j = 0; j < height; j++) {
+            if (j == 0){
+                map[i][j] = new Pion(i,j,1);
+            }else if (j == width-1){
+                map[i][j] = new Pion(i,j,2);
+            }else {
+                map[i][j] = new Pion(i,j,0);
+            }
+        }
+    }
+    console.log(map);
+    return map;
+}
+
+function DrawMap(){
+    createGrid(width, height, 50)
+    for (var i = 0; i < width; i++) {
+        for (var j = 0; j < height; j++) {
+            console.log(map[i][j].joueur );
+            if (map[i][j].joueur == 1){
+                drawSprite("Img/X.png", i, j);
+            }else if (map[i][j].joueur == 2){
+                drawSprite("Img/O.png", i, j);
+            }
+        }
+    }
+}
+
+function Init(){
+    map = CreateMap();
+    DrawMap(map);
+    createGrid(width, height);
+
+}
+
+
+Init();
